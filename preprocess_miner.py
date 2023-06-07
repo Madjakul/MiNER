4fdd11497c5285a5d3454d5aa29e99f502413b87
Francis Kulumba <fkulumba@ptb-02008441.paris.inria.fr>
2023-06-07 12:38:18 +0200

    Clean conll datasets.

--- preprocess_miner.py
+++ preprocess_miner.py
@@ -15,20 +15,12 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--lang", choices=["en", "fr"], default="en")
     parser.add_argument(
-        "--train_corpus_path",
+        "--corpus_path",
         type=str,
-        default="./data/bc5cdr/cdr_train_corpus.txt"
+        default="./data/bc5cdr/cdr_corpus.txt"
     )
     parser.add_argument(
-        "--val_corpus_path",
-        type=str,
-        default="./data/bc5cdr/cdr_val_corpus.txt"
-    )
-    parser.add_argument(
-        "--train_conll_path", type=str, default="./data/bc5cdr/cdr_train.conll"
-    )
-    parser.add_argument(
-        "--val_conll_path", type=str, default="./data/bc5cdr/cdr_val.conll"
+        "--conll_path", type=str, default="./data/bc5cdr/cdr_train.conll"
     )
     parser.add_argument(
         "--gazetteers_path", type=str, default="./data/gazetteers/"
@@ -40,12 +32,9 @@ if __name__=="__main__":
 
     logging.info("=== Mining Phrases ===")
 
-    logging.info(f"Reading training data from {args.train_corpus_path}")
-    with open(args.train_corpus_path, "r", encoding="utf-8") as f:
-        train_corpus = f.read().splitlines()
-    logging.info(f"Reading validation data from {args.val_corpus_path}")
-    with open(args.val_corpus_path, "r", encoding="utf-8") as f:
-        val_corpus = f.read().splitlines()
+    logging.info(f"Reading training data from {args.corpus_path}")
+    with open(args.corpus_path, "r", encoding="utf-8") as f:
+        corpus = f.read().splitlines()
 
     logging.info(f"Loading gazetteers from {args.gazetteers_path}")
     phrase_miner = PhraseMiner(lang=args.lang)
@@ -53,16 +42,14 @@ if __name__=="__main__":
     phrase_miner.compute_patterns(gazetteers)
     logging.info("Mining phrases...")
     unk_gazetteers = phrase_miner.get_unk_gazetteers(
-        corpus=train_corpus
+        corpus=corpus
     )
 
     logging.info(f"Dumping quality tokens to {args.unk_gazetteers_path}")
     with open(args.unk_gazetteers_path, "w", encoding="utf-8") as f:
         [f.write(f"{gazet}\n") for gazet in unk_gazetteers]
     phrase_miner.compute_patterns({"UNK": unk_gazetteers})
-    logging.info(f"Dumping training data to {args.train_conll_path}")
-    phrase_miner.dump(train_corpus, args.train_conll_path)
-    logging.info(f"Dumping validation data to {args.val_conll_path}")
-    phrase_miner.dump(val_corpus, args.val_conll_path)
+    logging.info(f"Dumping training data to {args.conll_path}")
+    phrase_miner.dump(corpus, args.conll_path)
     logging.info("--- Done ---\n\n")
 
--- preprocess_miner.sh
+++ preprocess_miner.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # LANG="en"
-# TRAIN_CORPUS_PATH=""
-# VAL_CORPUS_PATH=""
-# TRAIN_CONLL_PATH=""
-# VAL_CONLL_PATH=""
+# CORPUS_PATH=""
+# CONLL_PATH=""
 # GAZETTEERS_PATH=""
 # UNK_GAZETTEERS_PATH=""
 
@@ -18,10 +16,8 @@ mkdir tmp logs
 echo ${green}=== Mining Phrases ===${reset}
 python3 preprocess_miner.py \
     --lang ${LANG:-"en"} \
-    --train_corpus_path ${TRAIN_CORPUS_PATH:-"./data/bc5cdr/cdr_train_corpus.txt"} \
-    --val_corpus_path ${VAL_CORPUS_PATH:-"./data/bc5cdr/cdr_val_corpus.txt"} \
-    --train_conll_path ${TRAIN_CONLL_PATH:-"./data/bc5cdr/cdr_train.conll"} \
-    --val_conll_path ${VAL_CONLL_PATH:-"./data/bc5cdr/cdr_val.conll"} \
+    --corpus_path ${CORPUS_PATH:-"./data/bc5cdr/cdr_train_corpus.txt"} \
+    --conll_path ${CONLL_PATH:-"./data/bc5cdr/distant/cdr_train.conll"} \
     --gazetteers_path ${GAZETTEERS_PATH:-"./data/gazetteers/"} \
     --unk_gazetteers_path ${UNK_GAZETTEERS_PATH:-"./data/gazetteers/UNK.txt"}
 echo ${green}--- Done ---${reset}
